d689a12414dd06c948066ec061d3d37014b8fa19
Paul Farry <paulfarry@gmail.com>
2025-06-05 12:41:48 +1000

    Chapter 6 (local postgres)

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,31 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+//const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres('', {
+  host                 : process.env.POSTGRES_HOST || '',            // Postgres ip address[s] or domain name[s]
+  port                 : 5432,          // Postgres server port[s]
+  database             : process.env.POSTGRES_DATABASE || '',           // Name of database to connect to
+  username             : process.env.POSTGRES_USER || '',            // Username of database user
+  password             : process.env.POSTGRES_PASSWORD || '',            // Password of database user
+  ssl                  : false, 
+})
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
 
-// 	return data;
-// }
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+	return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -2,7 +2,20 @@ import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+//const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+/*
+//
+
+*/
+
+const sql = postgres('', {
+  host                 : process.env.POSTGRES_HOST || '',            // Postgres ip address[s] or domain name[s]
+  port                 : 5432,          // Postgres server port[s]
+  database             : process.env.POSTGRES_DATABASE || '',           // Name of database to connect to
+  username             : process.env.POSTGRES_USER || '',            // Username of database user
+  password             : process.env.POSTGRES_PASSWORD || '',            // Password of database user
+  ssl                  : false, 
+})
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
